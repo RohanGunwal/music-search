a824387ae8106421fa37fdce2ae07d24cbcecefb
Rohan <69361394+RohanGunwal@users.noreply.github.com>
2022-11-22 00:39:34 +0530

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import Search from './containers/Search';
-
-function App() {
-  return(
-    <Search title="Music Search App"/>
-  )}
-export default App;
